a0f6ac9bddb3b8dfe482aa109b791542c10f5096
AlexisPerdomoD <dropdibemol@gmail.com>
2024-03-31 22:28:13 -0500

    feat: add match cli arg to main function and use a method add, list or done properly

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::path::PathBuf;
-
 use structopt::StructOpt;
+use tasks::{add_task, Task};
+use cli::Action;
 
-use crate::tasks::{add_task, Task};
+use crate::tasks::{complete_task, get_list};
 mod tasks;
 mod cli;
 
+
+
+
 fn main() {
     let comands = cli::CommandLineArgs::from_args();
-    println!("{:#?}", comands);
-    let default_route:PathBuf = PathBuf::new();
-    println!("{:?}", default_route);
+    //temporary default route 
+    let default_route:PathBuf = PathBuf::from("data.json");
     let route = comands.route_file.unwrap_or(default_route);
     
     match  comands.action{
-         add => add_task(route, Task::new("prueba".to_string())),
-         list => todo!(),
-         Done => todo!()
-        };
+         Action::Add{task} => add_task(route, Task::new(task)),
+
+         Action::List => get_list(route),
+
+         Action::Done { position } => complete_task(route, position)
+
+    }
+    //temporally error management
+    .expect("faile to peformace");
 }
